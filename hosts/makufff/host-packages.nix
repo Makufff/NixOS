27c408e637a94f998231e5f6ae5b6ecc966a5207
Tanapat Chamted <91345773+Makufff@users.noreply.github.com>
2025-11-29 13:33:18 +0700

    add : postman

--- hosts/makufff/host-packages.nix
+++ hosts/makufff/host-packages.nix
@@ -1,19 +1,21 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    obsidian
-    protonvpn-gui # VPN
-    github-desktop
-    vscode
-    teams-for-linux
-    pkg-config
-    wayland
-    wayland-protocols
-    libxkbcommon
-  xorg.libX11
-  wireshark
-  wireshark-qt
-  nvtop
-    # pokego # Overlayed
-  ];
+  environment.systemPackages = with pkgs;
+    ( [
+      obsidian
+      protonvpn-gui # VPN
+      github-desktop
+      vscode
+      teams-for-linux
+      pkg-config
+      wayland
+      wayland-protocols
+      libxkbcommon
+      xorg.libX11
+      wireshark
+      wireshark-qt
+      code-cursor
+      # pokego # Overlayed
+      postman
+    ] ++ (if builtins.hasAttr "prismlauncher" pkgs then [ pkgs.prismlauncher ] else []));
 }
